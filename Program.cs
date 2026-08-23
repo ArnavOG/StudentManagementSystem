2f2ad78531d09e1382bffd968b1566b8077b2d59
ArnavOG <arnavbusinessfile@gmail.com>
2026-08-23 20:20:44 +0530

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Menu
+{
+    public void main_screen()
+    {
+        
+        System.Console.WriteLine("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+        System.Console.WriteLine("                   STUDENT MANAGEMENT SYSTEM ");
+        System.Console.WriteLine("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+
+        System.Console.WriteLine("\n1. Add Student \n2. View ");
+
+    }
+}
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+    }
+}
