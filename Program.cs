1b7e612da867aca1b591b106c17a15a4bfe6b32c
Rituraj shukla <shuklarituraj1902@gmail.com>
2026-08-24 20:43:34 +0530

    chas

--- Program.cs
+++ Program.cs
@@ -21,16 +21,9 @@ class Menu
 class Program
 {
     static void Main(string[] args)
-<<<<<<< HEAD
+
     {
        
 
-=======
-    {    
-        Menu m=new Menu();
-        Console.WriteLine("hello");
-        System.Console.WriteLine("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=" );
-        m.main_screen();
->>>>>>> 8e65ca4847f15ca721446ba7296f053d1ab8b71c
     }
 }
