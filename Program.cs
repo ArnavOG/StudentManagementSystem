360c68705aa0059f2e26f948a3ef6054149d6d73
Rituraj shukla <shuklarituraj1902@gmail.com>
2026-08-25 13:38:02 +0530

    hello

--- Program.cs
+++ Program.cs
@@ -4,16 +4,8 @@ class Menu
 {
     public void main_screen()
     {
-        
-        System.Console.WriteLine("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
-        System.Console.WriteLine("                   STUDENT MANAGEMENT SYSTEM ");
-        System.Console.WriteLine("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
-
-        System.Console.WriteLine("\n1. Add Student \n2. View All students \n3. Search Student \n4. Delete Student  \n5. Update Student \n6. Exit");
-        System.Console.WriteLine("\n");
-        System.Console.WriteLine("Enter your option");
-        int Choic;
-        Choic=Convert.ToInt32(Console.ReadLine());
+        System.Console.WriteLine("hello");
+       
     }
 }
 
@@ -21,8 +13,8 @@ class Menu
 class Program
 {
     static void Main(string[] args)
-
     {
-    
+       
+
     }
 }
