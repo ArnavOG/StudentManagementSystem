aa4c27991fe2caf38e05d0edd3ea5fd25cf8da0f
ArnavOG <arnavbusinessfile@gmail.com>
2026-08-25 13:30:59 +0530

    Comet

--- Program.cs
+++ Program.cs
@@ -5,18 +5,8 @@ class Menu
     public void main_screen()
     {
         
-        System.Console.WriteLine("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
-        System.Console.WriteLine("                   STUDENT MANAGEMENT SYSTEM ");
-        System.Console.WriteLine("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
-
-        System.Console.WriteLine("\n1. Add Student \n2. View All students \n3. Search Student \n4. Delete Student  \n5. Update Student \n6. Exit");
-        System.Console.WriteLine("\n");
-        System.Console.WriteLine("Enter your option");
-        int Choic;
-        Choic=Convert.ToInt32(Console.ReadLine());
     }
 }
-
 
 class Program
 {
