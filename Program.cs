bacbbf8003c861d6588c91671cc7b08a93c612e9
ItsSidPro <awasthisiddharth21@gmail.com>
2026-08-27 11:21:14 +0530

    Add console UI and input validation

--- Program.cs
+++ Program.cs
@@ -1,26 +1,108 @@
 ﻿using System;
 
+
 class Menu
-{
+{  
     public void main_screen()
-    {
+    {    
+
+        // START SCREEN 
+
+       System.Console.Clear();
+
+       System.Console.WriteLine("===================================================================");
+        
+        System.Console.WriteLine("               WELCOME TO COLLEGE MANAGEMENT SYSTEM               ");
+
+        System.Console.WriteLine("===================================================================");
+
+        System.Console.WriteLine();
+
+    
+        System.Console.WriteLine("                    PLEASE ENTER TO CONTINUE.........              ");
+        while (true)
+        {
+            ConsoleKeyInfo key = Console.ReadKey(true);
+
+            if (key.Key == ConsoleKey.Enter)
+            {
+                break;
+            }
+        }
+        
+
+        System.Console.Clear();
+
+
+               // MAIN MENU
         
         System.Console.WriteLine("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
-        System.Console.WriteLine("                   STUDENT MANAGEMENT SYSTEM ");
+        System.Console.WriteLine("                               MAIN MENU                                 ");
         System.Console.WriteLine("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
 
         System.Console.WriteLine("\n1. Add Student \n2. View All students \n3. Search Student \n4. Delete Student  \n5. Update Student \n6. Exit");
         System.Console.WriteLine("\n");
-        System.Console.WriteLine("Enter your option");
+       
+        
+                            // check valid choice
+
+        string choice;
+
+        while(true){
+         System.Console.WriteLine("Enter your choice");
+
+         choice = Console.ReadLine();
+
+        if (choice =="1"|| choice == "2" || choice =="3"|| choice =="4"|| choice =="5"|| choice =="6")
+        {
+            break;
+        }
+        
+            System.Console.WriteLine("Invalid choice!");
+            
+            System.Console.WriteLine("Please enter a number between 1 and 6");
+        }
+        
+            switch (choice)
+            {
+            case "1" :
+                System.Console.WriteLine("Add student ");
+             break;
+
+             case "2":
+                System.Console.WriteLine("View Students");
+            break;
+
+            case "3": 
+                System.Console.WriteLine( "Search Student ");
+            break;
+
+            case "4": 
+                System.Console.WriteLine( "Search Student ");
+            break;
+            
+              case "5": 
+                System.Console.WriteLine( "Search Student ");
+            break;
+
+              case "6": 
+                System.Console.WriteLine( "Search Student ");
+            break;
+     
+                          return;
+            }
+        }
     }
-}
+
 
 
 class Program
 {
     static void Main(string[] args)
+
     {
-        Menu mn =new Menu();
-        mn.main_screen();
+        Menu mu =new Menu();
+
+        mu.main_screen();
     }
-}
+} 
--- obj/Debug/net10.0/StudentManagementSystem.AssemblyInfo.cs
+++ obj/Debug/net10.0/StudentManagementSystem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12a89ecccc144ebf220b351115b9b9badefec234")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c285f95d422e604a9fcc7f83caedbe25ff567493")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
