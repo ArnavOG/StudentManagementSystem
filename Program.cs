2c6f2eb6c7034ad020d3413da8f2b0c6f7f28286
Siddharth Awasthi <awasthisiddharth21@gmail.com>
2026-08-28 12:37:36 +0530

    Fix casing of WriteLine method calls

--- Program.cs
+++ Program.cs
@@ -9,7 +9,8 @@ class Menu
         // START SCREEN 
 
        System.Console.Clear();
-
+       System.Console.writeLin("_____________________________________________________________________________________________________________________________");
+       System.Console.writeLin("\n");
        System.Console.WriteLine("===================================================================");
         
         System.Console.WriteLine("     ⚫     🏫 WELCOME TO COLLEGE MANAGEMENT SYSTEM               ");
@@ -25,6 +26,10 @@ class Menu
        
         System.Console.WriteLine("\n");
         System.Console.WriteLine("                          👉   [ ENTER ]   👈                         ");
+
+        System.Console.writeLin("\n");
+        System.Console.writeLin("____________________________________________________________________________________________________________________________");
+        System.Console.writeLin("\n");
         while (true)
         {
             ConsoleKeyInfo key = Console.ReadKey(true);
