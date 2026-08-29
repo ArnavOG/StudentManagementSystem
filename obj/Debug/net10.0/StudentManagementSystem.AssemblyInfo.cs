40d7c4779fbab2f014f09ba4bb82d382708a00f2
ItsSidPro <awasthisiddharth21@gmail.com>
2026-08-29 12:29:52 +0530

    Update

--- obj/Debug/net10.0/StudentManagementSystem.AssemblyInfo.cs
+++ obj/Debug/net10.0/StudentManagementSystem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f13c2a30630af19e2dc888e6a3027c437aeed39e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b63b78d1fbf1a5f71bc341de7cdecebdd9139d9f")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
